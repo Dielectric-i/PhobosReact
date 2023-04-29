e842cb674b2377f4beed745a437a82b128a2fac3
Dielectric-i <ruesinc@gmail.com>
2023-04-29 17:15:50 +0600

    initReduxToolkit

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -5,14 +5,18 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { store } from './store/store';
+import {Provider } from 'react-redux'
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
+    <BrowserRouter basename={baseUrl}>
+        <Provider store={store }>
+            <App />
+        </Provider>
   </BrowserRouter>);
 
 // If you want your app to work offline and load faster, you can change
